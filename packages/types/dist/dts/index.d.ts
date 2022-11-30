f5d9a1b8bb1516810630ddfd294803b0a67f26b6
pete_the_pete <pliljegr@gmail.com>
2022-11-30 09:11:24 -0800

    Adding typescript-eslint for types package

--- packages/types/dist/dts/index.d.ts
+++ packages/types/dist/dts/index.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="node" />
-import { PlatformPath } from 'path';
+import { PlatformPath } from "path";
 export interface Plugin {
-    onConfigure: (options: ProjectOptions[]) => any;
-    onProjectAverage: (projectReport: any, pathModule: PlatformPath, settings: any) => any;
-    onProjectCalculate: (projectReport: any, pathModule: PlatformPath, settings: any) => any;
-    onProjectEnd: (projectReport: any, pathModule: PlatformPath, settings: any) => any;
-    onProjectPostAverage: (projectReport: any, pathModule: PlatformPath, settings: any) => any;
-    onProjectStart: (projectReport: any, pathModule: PlatformPath, settings: any) => any;
+    onConfigure(options: ProjectOptions[]): any;
+    onProjectAverage(projectReport: any, pathModule: PlatformPath, settings: any): any;
+    onProjectCalculate(projectReport: any, pathModule: PlatformPath, settings: any): any;
+    onProjectEnd(projectReport: any, pathModule: PlatformPath, settings: any): any;
+    onProjectPostAverage(projectReport: any, pathModule: PlatformPath, settings: any): any;
+    onProjectStart(projectReport: any, pathModule: PlatformPath, settings: any): any;
 }
 export interface PluginOptions {
     loadDefaultPlugins: boolean;
